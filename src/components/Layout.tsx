414eb2fe4ae5e734098809b1f04d7549758d667e
Stephan Griesel <sgriesel@gmail.com>
2022-12-21 05:36:49 +0100

    header nav update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,22 +15,8 @@ const Layout = ({ children }) => {
     <>
       <header className="py-6 md:py-12">
         <div className="max-w-6xl mx-auto px-6">
-          <div className="flex items-center justify-between">
-            <div className="md:w-1/3">
-              <nav className="flex items-center justify-start space-x-3 md:space-x-6">
-                <Link href="/about">
-                  <a className="text-gray-800 hover:text-blue-600 p-1 transition">
-                    About
-                  </a>
-                </Link>
-                {/* <Link href="/terms-of-sale">
-                  <a className="text-gray-800 hover:text-blue-600 p-1 transition">
-                    Terms of Sale
-                  </a>
-                </Link> */}
-              </nav>
-            </div>
-            <div className="flex-1 flex items-center justify-center">
+          <div className="flex items-center justify-evenly">
+            <div className="flex-1 flex px-6">
               <Link href="/">
                 <a className="flex items-center text-gray-900">
                   <div className="group">
